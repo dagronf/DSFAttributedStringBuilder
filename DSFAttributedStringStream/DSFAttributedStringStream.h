b13239b3314abb164cf1ad3bfe88674fd614cb9f
Darren Ford <dford_au-reg@yahoo.com>
2019-03-26 09:37:52 +1100

    Initial Commit

---
+++ DSFAttributedStringStream/DSFAttributedStringStream.h
@@ -0,0 +1,19 @@
+//
+//  DSFAttributedStringStream.h
+//  DSFAttributedStringStream
+//
+//  Created by Darren Ford on 26/3/19.
+//  Copyright © 2019 Darren Ford. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DSFAttributedStringStream.
+FOUNDATION_EXPORT double DSFAttributedStringStreamVersionNumber;
+
+//! Project version string for DSFAttributedStringStream.
+FOUNDATION_EXPORT const unsigned char DSFAttributedStringStreamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DSFAttributedStringStream/PublicHeader.h>
+
+
